a7820095ab45a505b88e3c05c10d75d0d7252fe3
DuyLuu <buiduyluu@192.168.2.46>
2025-05-13 10:17:51 +0700

    removing redundant code

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <View> <Text>Hello</Text></View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
